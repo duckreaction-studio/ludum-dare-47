ec10fa676b10687945d1510f6cd0336dbc3fdc08
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-10-03 18:40:47 +0200

    Test explosion cow

---
+++ ludum-dare-47/Assets/Scripts/Cow/Cow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem explosion;
+    [SerializeField]
+    private Transform body;
+
+    public void Awake()
+    {
+        explosion.gameObject.SetActive(false);
+    }
+
+    public void Die()
+    {
+        body.gameObject.SetActive(false);
+        explosion.gameObject.SetActive(true);
+        explosion.loop = false;
+        explosion.Play();
+    }
+}
