4890835022897c74f05aa26ff001d419db22f4c3
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-10-03 17:41:17 +0200

    Lasso can hit a cow

---
+++ ludum-dare-47/Assets/Scripts/Cow/CowMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CowMovementController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField]
+    private float walkSpeed;
+
+    private CharacterController characterController;
+   
+   
+    void Start()
+    {
+        characterController = GetComponentInChildren<CharacterController>();
+    }
+
+    private void Update()
+    {
+
+        Vector3 dir = Vector3.zero;
+        dir += Physics.gravity;
+        characterController.Move( dir * Time.deltaTime);
+    }
+
+  
+}
---
+++ ludum-dare-47/Assets/Scripts/FPS/Lasso.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lasso : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInputController playerInputController;
+    [SerializeField]
+    private float maxDistance = 5f;
+
+    private Camera fpsCamera;
+    private int layerMask;
+    void Start()
+    {
+        fpsCamera = GetComponent<Camera>();
+        layerMask = LayerMask.NameToLayer("CowTarget");
+
+        playerInputController.inputActions.Player.Fire.performed += OnFire;
+    }
+
+    private void OnFire(InputAction.CallbackContext obj)
+    {
+        if (obj.performed)
+        {
+            Debug.Log("Fire");
+            Ray ray = new Ray(fpsCamera.transform.position, fpsCamera.transform.forward);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, maxDistance, layerMask))
+            {
+                Debug.Log("Get a cow");
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
