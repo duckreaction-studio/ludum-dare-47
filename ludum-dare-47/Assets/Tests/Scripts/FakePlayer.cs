6dfb4291e4986e6abcc4f59720b473b376e0fd5a
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-10-03 23:26:07 +0200

    Test navmesh

---
+++ ludum-dare-47/Assets/Tests/Scripts/FakePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class FakePlayer : MonoBehaviour, IPlayer
+{
+    CowAI testCow;
+
+    [Inject]
+    public void Construct(CowAI cow)
+    {
+        testCow = cow;
+    }
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    [ContextMenu("Test nav mesh")]
+    public void TestNavMesh()
+    {
+        testCow.StartFollowPlayer();
+    }
+}
