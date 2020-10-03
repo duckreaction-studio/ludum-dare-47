ed9bbe8dd0614ebec37cf46d46b60bf8ed726b34
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-10-03 16:04:18 +0200

    Add FPS System WIP

---
+++ ludum-dare-47/Assets/Scripts/FPS/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraController : MonoBehaviour
+{
+   public enum ADSType
+    {
+        HoldToAim,
+        ClickToAim
+    }
+    
+    [Header("Camera target position")]
+    [SerializeField]
+    private Transform Target;
+
+    [Header("Camera Aim Settings")]
+    [SerializeField]
+    private float VerticalSensitivity;
+    [SerializeField]
+    private float HorizontalSensitivity;
+    [SerializeField]
+    private float MaxLookUpAngle;
+    [SerializeField]
+    private float MinLookUpAngle;
+
+    Vector2 mouseInput;
+    Vector2 rotation;
+    public PlayerInputController playerInputController;
+    
+
+    private void Update()
+    {
+        mouseInput = playerInputController.inputActions.Player.Look.ReadValue<Vector2>();
+        rotation.x += mouseInput.x * HorizontalSensitivity;
+        rotation.y += mouseInput.y * VerticalSensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, MinLookUpAngle, MaxLookUpAngle);
+        transform.localRotation = Quaternion.Euler(-rotation.y, rotation.x, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Target.position;
+    }
+
+}
