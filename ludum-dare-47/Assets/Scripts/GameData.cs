54cd97185269b83be64b9b8aefab2da72e60690e
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-10-04 15:25:07 +0200

    Add UI

---
+++ ludum-dare-47/Assets/Scripts/GameData.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class GameOver
+{
+
+}
+
+public class GameRestart
+{
+
+}
+
+public class GameData : ITickable
+{
+    public static readonly int MAX_LIFE = 100;
+    public static readonly int SCORE_PER_COW = 100;
+    public static readonly int SCORE_PER_SECOND = 10;
+
+    public int score 
+    {
+        get
+        {
+            return (int)(_timeScore * SCORE_PER_SECOND + _cowScore * SCORE_PER_COW);
+        }
+    }
+    public float life { get; private set; } = 100;
+    public float relativeLife
+    {
+        get
+        {
+            return life / MAX_LIFE;
+        }
+    }
+
+    public bool pause { get; set; }
+    public bool end { get; private set; }
+
+    private float _timeScore;
+    private int _cowScore;
+    private SignalBus _signalBus;
+    private CompositeDisposable _disposables = new CompositeDisposable();
+
+    public GameData(SignalBus signalBus)
+    {
+        _signalBus = signalBus;
+        _signalBus.GetStream<GameRestart>()
+            .Subscribe(x => Restart())
+            .AddTo(_disposables);
+    }
+
+    public void Restart()
+    {
+        life = MAX_LIFE;
+        _timeScore = 0;
+        _cowScore = 0;
+        end = false;
+        pause = false;
+    }
+
+    public void Tick()
+    {
+        if(!pause && !end)
+        {
+            _timeScore += Time.deltaTime;
+            life -= Time.deltaTime;
+            if(life <= 0)
+            {
+                end = true;
+                _signalBus.Fire<GameOver>();
+            }
+        }
+    }
+
+    public void PlayerCatchACow()
+    {
+        _cowScore++;
+        life += 10;
+        life = Mathf.Min(life, 100);
+    }
+}
---
+++ ludum-dare-47/Assets/Scripts/UI/OverlayUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class OverlayUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image lifeBar;
+    [SerializeField]
+    private Text score;
+
+    private GameData _gameData;
+
+    [Inject]
+    public void Construct(GameData gameData)
+    {
+        _gameData = gameData;
+        lifeBar.fillAmount = 1;
+        score.text = "";
+    }
+
+    public void Update()
+    {
+        lifeBar.fillAmount = _gameData.relativeLife;
+        score.text = _gameData.score.ToString();
+    }
+}
